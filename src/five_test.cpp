d1b0d32e56a498a6064bce7288d2a249072952a5
Heavenfiend <EGS-76@yandex.ru>
2025-10-02 11:14:32 +0300

    123

--- src/five_test.cpp
+++ src/five_test.cpp
@@ -1,85 +1,73 @@
 #include "Five.h"
 #include "gtest/gtest.h"
 
-TEST (FiveBasics, ConstructFromString)
-{
-    Five a ("42");
-    EXPECT_EQ (a.toDecimal (), 22);
+TEST(FiveTest, ConstructFromString) {
+    Five a("42");
+    EXPECT_EQ(a.toDecimal(), 22);
 }
 
-TEST (FiveBasics, ConstructFromDecimal)
-{
-    Five a (22);
-    EXPECT_EQ (a.toDecimal (), 22);
+TEST(FiveTest, ConstructFromDecimal) {
+    Five a(22);
+    EXPECT_EQ(a.toDecimal(), 22);
 }
 
-TEST (FiveArithmetic, Add)
-{
-    Five a ("42");
-    Five b ("34");
-    Five c = a.add (b);
-    EXPECT_EQ (c.toDecimal (), 41);
+TEST(FiveTest, AddTwoNumbers) {
+    Five a("42");
+    Five b("34");
+    Five c = a.add(b);
+    EXPECT_EQ(c.toDecimal(), 41);
 }
 
-TEST (FiveArithmetic, Sub)
-{
-    Five a ("42");
-    Five b ("34");
-    Five c = a.subtract (b);
-    EXPECT_EQ (c.toDecimal (), 3);
+TEST(FiveTest, SubtractTwoNumbers) {
+    Five a("42");
+    Five b("34");
+    Five c = a.subtract(b);
+    EXPECT_EQ(c.toDecimal(), 3);
 }
 
-TEST (FiveArithmetic, SubNegative)
-{
-    Five a ("34");
-    Five b ("42");
-    Five c = a.subtract (b);
-    EXPECT_EQ (c.toDecimal (), 0);
+TEST(FiveTest, SubtractResultNegativeReturnsZero) {
+    Five a("34");
+    Five b("42");
+    Five c = a.subtract(b);
+    EXPECT_EQ(c.toDecimal(), 0);
 }
 
-TEST (FiveCompare, LessThan)
-{
-    Five a ("34");
-    Five b ("42");
-    EXPECT_TRUE (a.less (b));
+TEST(FiveTest, LessThanComparison) {
+    Five a("34");
+    Five b("42");
+    EXPECT_TRUE(a.less(b));
 }
 
-TEST (FiveCompare, GreaterThan)
-{
-    Five a ("42");
-    Five b ("34");
-    EXPECT_TRUE (a.greater (b));
+TEST(FiveTest, GreaterThanComparison) {
+    Five a("42");
+    Five b("34");
+    EXPECT_TRUE(a.greater(b));
 }
 
-TEST (FiveCompare, Equals)
-{
-    Five a ("42");
-    Five b ("42");
-    EXPECT_TRUE (a.equals (b));
+TEST(FiveTest, EqualComparison) {
+    Five a("42");
+    Five b("42");
+    EXPECT_TRUE(a.equals(b));
 }
 
-TEST (FiveErrors, InvalidString)
-{
-    EXPECT_THROW (Five ("125"), std::invalid_argument);
-    EXPECT_THROW (Five ("abc"), std::invalid_argument);
+TEST(FiveTest, InvalidStringThrows) {
+    EXPECT_THROW(Five("125"), std::invalid_argument);
+    EXPECT_THROW(Five("abc"), std::invalid_argument);
 }
 
-TEST (FiveImmutability, OriginalUnchangedAfterOperations)
-{
-    Five original ("123");
-    std::string originalStr = original.toString ();
+TEST(FiveTest, OriginalUnchangedAfterOperations) {
+    Five original("123");
+    std::string originalStr = original.toString();
 
-    Five sum = original.add (Five ("1"));
-    Five diff = original.subtract (Five ("1"));
-    Five assigned = original.assign (Five ("333"));
+    Five sum = original.add(Five("1"));
+    Five diff = original.subtract(Five("1"));
+    Five assigned = original.assign(Five("333"));
 
-    EXPECT_EQ (original.toString (), originalStr);
-    EXPECT_EQ (original.toString (), "123");
+    EXPECT_EQ(original.toString(), originalStr);
+    EXPECT_EQ(original.toString(), "123");
 }
 
-int
-main (int argc, char **argv)
-{
-    ::testing::InitGoogleTest (&argc, argv);
-    return RUN_ALL_TESTS ();
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
